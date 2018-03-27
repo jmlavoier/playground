ddb6cc6e5ebf233c55cdf040be374ca2b64a00f5
Joao Lavoier <jmlavoier@hotmail.com>
2018-03-27 09:14:14 -0300

    Live Code

---
+++ react-16/gdg-presentation.js
@@ -0,0 +1,13 @@
+
+const App = () => (
+   <div>
+      <h1 className="center">Hello GDG!</h1>
+      <div className="container">
+        <div className="box color-2">
+          O que há de novo no React@16?
+        </div>
+      </div>
+   </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
